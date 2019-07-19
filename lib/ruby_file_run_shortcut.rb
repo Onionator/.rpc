334664123c03b342c71262e064cdac2963409da3
Sam Cook <WhenPotatoesFry@gmail.com>
2019-07-18 23:02:55 -0700

    refine the ruby files and test.

--- lib/ruby_file_run_shortcut.rb
+++ lib/ruby_file_run_shortcut.rb
@@ -1,8 +1,12 @@
 require("fileutils")
-puts "If the file you want to open is the first in this list enter 1 or if it is the second enter 2..."
+clown_file = File.open("../rpc/clown.txt")
+clown = clown_file.read
+clown_file.close
+puts "~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~\n#{clown}\n~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~\nEnter the number of the ruby file you want to run.\n~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~"
 file_to_run = Dir.glob("lib/*.rb")
-file_to_run.each { |files|
-  puts files
+file_to_run.each_with_index { |files, i|
+  puts "#{i + 1}. #{files}"
 }
 number = gets.chomp
+puts "~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~"
 system "ruby #{file_to_run[number.to_i - 1]}"
